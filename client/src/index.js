bbf051b759ff90931b2863ae85f38e36473e4376
ryanbyloos <ryan.byloos@posteo.net>
2022-05-16 10:16:40 +0200

    add router

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
-import './index.css';
+import { ChakraProvider, Heading } from '@chakra-ui/react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider>
-    <Login />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Heading>SSChat</Heading>} />
+        <Route path="/login" element={ <Login /> } />
+      </Routes>
+    </BrowserRouter>
   </ChakraProvider>
 );
