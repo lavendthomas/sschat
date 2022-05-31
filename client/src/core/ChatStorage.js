464f9ceba002507688be9ebe2dd933f7c4e9ef62
ryanbyloos <ryan.byloos@posteo.net>
2022-05-31 17:28:34 +0200

    ChatStorage JSdoc

--- client/src/core/ChatStorage.js
+++ client/src/core/ChatStorage.js
@@ -1,7 +1,15 @@
 import Dexie from "dexie";
 const LOCALSTORAGE_KEY = "chat-storage";
 
+/**
+ * @class ChatStorage
+ * @classdesc The ChatStorage class is a wrapper for the Dexie local storage.
+ */
 class ChatStorage {
+  /**
+   * The ChatStorage constructor.
+   * @param {*} us The user object.
+   */
   constructor(us) {
     this.us = us;
     // this.load();
@@ -11,6 +19,12 @@ class ChatStorage {
     });
   }
 
+  /**
+   * This method loads the chat history from the local storage.
+   * @param {*} from_user The user object.
+   * @param {*} to_user The recipient object.
+   * @param {*} updateMessageList The function to call when the message list is updated.
+   */
   async get_messages(from_user, to_user, updateMessageList) {
     try {
       const messages_from = await this.db.chat
@@ -26,6 +40,13 @@ class ChatStorage {
     } catch (e) {}
   }
 
+  /**
+   * This method add a message to the local storage.
+   * @param {*} from_user The user object.
+   * @param {*} to_user The recipient object.
+   * @param {*} message The message object.
+   * @param {*} timestamp The timestamp of the message.
+   */
   async add_message(from_user, to_user, message, timestamp) {
     try {
       await this.db.chat.add({
